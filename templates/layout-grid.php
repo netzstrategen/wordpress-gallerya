40f74ef0109e95f087c56c62d30255a8e17c8da8
Fabian Marz <fabian@netzstrategen.com>
2016-07-29 10:24:03 +0200

    Fixed output caption only if available.

--- templates/layout-grid.php
+++ templates/layout-grid.php
@@ -4,13 +4,15 @@ namespace Netzstrategen\Gallerya;
 
 <div class="gallerya gallerya--grid">
   <ul class="js-gallerya-slider js-gallerya-lightbox">
-    <?php foreach ($images as $image): ?>
+    <?php foreach ($images as $image):
+      $caption = apply_filters('gallerya/image_caption', $image->post_excerpt, $image->ID);
+    ?>
       <li>
         <figure class="gallerya__image">
-          <a href="<?= wp_get_attachment_image_src($image->ID, apply_filters('gallerya/image_size_lightbox', 'large'))[0] ?>" data-sub-html=".gallerya__image__caption">
+          <a href="<?= wp_get_attachment_image_src($image->ID, apply_filters('gallerya/image_size_lightbox', 'large'))[0] ?>" <?= !empty($caption) ? 'data-sub-html=".gallerya__image__caption"' : '' ?>>
             <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_grid', 'medium')) ?>
-          <?php if ($image->post_excerpt): ?>
-            <figcaption class="gallerya__image__caption"><?= apply_filters('gallerya/image_caption', $image->post_excerpt, $image->ID) ?></figcaption>
+          <?php if (!empty($caption)): ?>
+            <figcaption class="gallerya__image__caption"><?= $caption ?></figcaption>
           <?php endif; ?>
           </a>
         </figure>
