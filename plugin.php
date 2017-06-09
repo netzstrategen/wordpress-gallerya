6c3e1fc1a8e1d5f79bbccd60cb3311d667fd2f8e
Timo Gauweiler <timo@netzstrategen.com>
2017-06-09 16:22:46 +0200

    Fixed margins of slider image due to third-party plugin overrides.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Gallerya
-  Version: 1.9.3
+  Version: 1.9.4
   Text Domain: gallerya
   Description: Change the native post gallery to be displayed as a slider with lightbox support.
   Author: netzstrategen
