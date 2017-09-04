94342d6367e8a8f09b75a58486aed707a0b4c942
Joan López <joan@netzstrategen.com>
2017-09-04 18:28:13 +0200

    fixup! Fixed Add woocommerce v3 lightbox support to the active theme.

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -16,11 +16,6 @@ class WooCommerce {
    * @implements admin_init
    */
   public static function init() {
-    // Add WooCommerce v3.x Gallery Lightbox support
-    // @see https://woocommerce.wordpress.com/2017/02/28/adding-support-for-woocommerce-2-7s-new-gallery-feature-to-your-theme/
-    // @see https://github.com/woocommerce/woocommerce/wiki/Enabling-product-gallery-features-(zoom,-swipe,-lightbox)-in-3.0.0
-    add_theme_support('wc-product-gallery-lightbox');
-
     add_filter('pre_option_woocommerce_enable_lightbox', function () { return 'no'; });
     add_filter('woocommerce_product_settings', __CLASS__ . '::woocommerce_product_settings');
   }
