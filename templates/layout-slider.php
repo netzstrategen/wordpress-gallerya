fe3df4b65c314e71062de9d8f2e1b8f875f8eb50
Joan López <joan@netzstrategen.com>
2017-11-26 20:48:06 +0100

    Fixed gallerya slider display issues when wp-rocket plugin lazyload option is set.
    Gallerya sliders collapse vertically if plugin wp-rocket is in use and
    configured to lazyload the images. This fixes the issue by disabling
    the lazyload option for the images displayed in the gallerya slider.

--- templates/layout-slider.php
+++ templates/layout-slider.php
@@ -3,31 +3,36 @@ namespace Netzstrategen\Gallerya;
 
 $show_navigation = count($images) >= $nav_count_min;
 $slider_image_size = has_image_size('post-thumbnail') ? 'post-thumbnail' : 'large';
+
+$image_attr = [];
 ?>
 
 <div class="gallerya gallerya--slider">
   <ul class="js-gallerya-slider js-gallerya-lightbox">
+    <?php $image_attr = Plugin::lazyLoadIsActive() ? ['data-no-lazy' => '1'] : []; ?>
     <?php foreach ($images as $image):
       $caption = apply_filters('gallerya/image_caption', $image->post_excerpt, $image->ID);
     ?>
       <li>
         <figure class="gallerya__image">
           <a href="<?= wp_get_attachment_image_src($image->ID, apply_filters('gallerya/image_size_lightbox', 'large'))[0] ?>" <?= !empty($caption) ? 'data-sub-html="' . esc_attr($caption) . '"' : '' ?>>
-            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider', $slider_image_size)) ?>
+            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider', $slider_image_size), FALSE, $image_attr) ?>
           <?php if (!empty($caption)): ?>
             <figcaption class="gallerya__image__caption"><?= $caption ?></figcaption>
           <?php endif; ?>
           </a>
         </figure>
       </li>
+      <?php $image_attr = []; ?>
     <?php endforeach; ?>
   </ul>
   <?php if ($show_navigation): ?>
     <ul class="gallerya--slider__nav  js-gallerya-thumbnail-slider">
+      <?php $image_attr = Plugin::lazyLoadIsActive() ? ['data-no-lazy' => '1'] : []; ?>
       <?php foreach ($images as $image): ?>
         <li>
           <figure class="gallerya__image">
-            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider_thumbnail', 'thumbnail')) ?>
+            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider_thumbnail', 'thumbnail'), FALSE, $image_attr) ?>
           </figure>
         </li>
       <?php endforeach; ?>
