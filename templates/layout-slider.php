1acc328e2645d1ec4084c2070b513c14f872af42
Joan López <joan@netzstrategen.com>
2017-11-27 17:13:35 +0100

    fixup! Fixed gallerya slider display issues when wp-rocket plugin lazyload option is set.

--- templates/layout-slider.php
+++ templates/layout-slider.php
@@ -3,20 +3,18 @@ namespace Netzstrategen\Gallerya;
 
 $show_navigation = count($images) >= $nav_count_min;
 $slider_image_size = has_image_size('post-thumbnail') ? 'post-thumbnail' : 'large';
-
-$image_attr = [];
 ?>
 
 <div class="gallerya gallerya--slider">
   <ul class="js-gallerya-slider js-gallerya-lightbox">
     <?php $image_attr = Plugin::lazyLoadIsActive() ? ['data-no-lazy' => '1'] : []; ?>
-    <?php foreach ($images as $image):
+    <?php foreach ($images as $index => $image):
       $caption = apply_filters('gallerya/image_caption', $image->post_excerpt, $image->ID);
     ?>
       <li>
         <figure class="gallerya__image">
           <a href="<?= wp_get_attachment_image_src($image->ID, apply_filters('gallerya/image_size_lightbox', 'large'))[0] ?>" <?= !empty($caption) ? 'data-sub-html="' . esc_attr($caption) . '"' : '' ?>>
-            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider', $slider_image_size), FALSE, $image_attr) ?>
+            <?= wp_get_attachment_image($image->ID, apply_filters('gallerya/image_size_slider', $slider_image_size), FALSE, !$index ?: $image_attr) ?>
           <?php if (!empty($caption)): ?>
             <figcaption class="gallerya__image__caption"><?= $caption ?></figcaption>
           <?php endif; ?>
@@ -28,7 +26,6 @@ $image_attr = [];
   </ul>
   <?php if ($show_navigation): ?>
     <ul class="gallerya--slider__nav  js-gallerya-thumbnail-slider">
-      <?php $image_attr = Plugin::lazyLoadIsActive() ? ['data-no-lazy' => '1'] : []; ?>
       <?php foreach ($images as $image): ?>
         <li>
           <figure class="gallerya__image">
