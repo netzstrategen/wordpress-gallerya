8bb642d9a73480bdae75c1256f4a2884b8e4f03e
Luca Pipolo <luca@netzstrategen.com>
2018-02-16 08:39:53 +0100

    Fixed other images than products appear in lightgallery. (#14)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -50,7 +50,7 @@
         thumbnail: true,
         showThumbByDefault: false,
         subHtmlSelectorRelative: true,
-        selector: 'a'
+        selector: '.woocommerce-product-gallery__image > a'
       });
     }
 
