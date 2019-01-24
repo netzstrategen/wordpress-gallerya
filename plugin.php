0c407a7a3db9a84f800c187270ba5e8e58e35f9a
Florent <florent.zambon@gmail.com>
2019-01-24 14:02:54 +0100

    Updated Gallerya to 1.10.1.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Gallerya
-  Version: 1.10.1
+  Version: 1.10.2
   Text Domain: gallerya
   Description: Change the native post gallery to be displayed as a slider with lightbox support.
   Author: netzstrategen
