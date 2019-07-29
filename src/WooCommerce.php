02ebb4f72ac863bf8ec7dd87cb7201c362623079
Evelia Molina <evelia@netzstrategen.com>
2019-07-29 10:14:25 +0200

    Fixed displayed image size in woocommerce product gallery.

--- src/WooCommerce.php
+++ src/WooCommerce.php
@@ -14,6 +14,7 @@ class WooCommerce {
 
   /**
    * Adds data-srcset attributes to image links to make them reponsive in lightGallery.
+   *    * Adds data-sizes attributes to image links so they don't get enlarged more than the original size.
    *
    * @see https://sachinchoolur.github.io/lightGallery/demos/responsive.html
    *
@@ -21,7 +22,8 @@ class WooCommerce {
    */
   public static function woocommerce_single_product_image_thumbnail_html($html, $thumbnail_id) {
     $srcset = wp_get_attachment_image_srcset($thumbnail_id, 'shop_single');
-    return preg_replace('/(<a\s+)/i', '<a data-srcset="' . $srcset . '" ', $html);
+    $srcsizes = wp_get_attachment_image_sizes($thumbnail_id, 'large');
+    return preg_replace('/(<a\s+)/i', '<a data-srcset="' . $srcset . '" data-sizes="' . $srcsizes . '" ', $html);
   }
 
 }
