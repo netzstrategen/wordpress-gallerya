d2523b08ff90a99b246c43011119bdb3aab25c1d
Joan López <joan@netzstrategen.com>
2020-03-31 10:13:29 +0200

    Bumped plugin version. (#46)

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Gallerya
-  Version: 2.2.7
+  Version: 2.2.8
   Text Domain: gallerya
   Description: Change the native post gallery to be displayed as a slider with lightbox support.
   Author: netzstrategen
