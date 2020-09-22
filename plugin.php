b856b4aa8446367d5c894c4c0434de9021b2cf01
Joan López <joan@netzstrategen.com>
2020-09-22 14:38:25 +0200

    fixup! Fixed PHP notice thrown when retrieving thumbnails of variable product with no variations.

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Gallerya
-  Version: 2.7.2
+  Version: 2.7.3
   Text Domain: gallerya
   Description: Change the native post gallery to be displayed as a slider with lightbox support.
   Author: netzstrategen
