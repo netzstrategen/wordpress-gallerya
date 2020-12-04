8b368331d0d1e7692cd21e22fdc0a033187d1822
Joan López <joan@netzstrategen.com>
2020-12-04 19:00:47 +0100

    fixup! Fixed keyboard navigation sync between product slider and lightbox slider

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Gallerya
-  Version: 2.7.10
+  Version: 2.7.11
   Text Domain: gallerya
   Description: Change the native post gallery to be displayed as a slider with lightbox support.
   Author: netzstrategen
