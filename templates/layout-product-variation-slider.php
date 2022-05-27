2d843c2d04e37fd86d8e7481173e1d1268b241a4
Bogdan Arizancu <bogdan@netzstrategen.com>
2022-05-27 15:16:37 +0300

    Fixed variation slider does not load correct image if srcset is not available. (#85)

--- templates/layout-product-variation-slider.php
+++ templates/layout-product-variation-slider.php
@@ -10,14 +10,15 @@ $transparent_pixel = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAA
 
 <div class="gallerya gallerya--product-variation-slider" data-gallerya-page-dots="<?= (int) $show_page_dots ?>">
   <ul class="js-gallerya-slider">
-    <?php foreach ($attachment_ids as $index => $attachment_id): ?>
+    <?php foreach ($attachment_ids as $index => $attachment_id):
+      $slider_image_srcset = wp_get_attachment_image_srcset($attachment_id, $slider_image_src); ?>
       <li data-gallerya-variant-permalink="<?= $variation_permalinks[$index] ?>">
         <figure class="gallerya__image">
           <?= wp_get_attachment_image($attachment_id, $slider_image_src, FALSE, $index ? [
             'src' => $transparent_pixel,
-            'srcset' => $transparent_pixel,
+            'srcset' => $slider_image_srcset ? $transparent_pixel : '',
             'data-flickity-lazyload-src' => wp_get_attachment_image_url($attachment_id, $slider_image_src),
-            'data-flickity-lazyload-srcset' => wp_get_attachment_image_srcset($attachment_id, $slider_image_src),
+            'data-flickity-lazyload-srcset' => $slider_image_srcset,
           ] : [
             'loading' => 'lazy',
           ]) ?>
