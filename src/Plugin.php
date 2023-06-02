466647c628b9c9dce36329e7efad0a5f94ffc9e7
creative-andrew <andres@netzstrategen.com>
2023-06-02 13:01:19 +0200

    Fixed incompatible  markup with plugin woo-product-gallery-slider.

--- src/Plugin.php
+++ src/Plugin.php
@@ -78,8 +78,10 @@ class Plugin {
       // @todo Make lightGallery properly respect srcset & sizes in JavaScript
       // instead of duplicating that information in HTML.
       // @see https://github.com/netzstrategen/wordpress-gallerya/pull/11#issuecomment-355664739
-      add_filter('woocommerce_single_product_image_thumbnail_html', __NAMESPACE__ . '\WooCommerce::woocommerce_single_product_image_thumbnail_html', 10, 2);
-
+      // Only add the filter if the plugin woo-product-gallery-slider is not active as it already include this and creates a conflict.
+      if (!is_plugin_active('woo-product-gallery-slider/woo-product-gallery-slider.php')) {
+        add_filter('woocommerce_single_product_image_thumbnail_html', __NAMESPACE__ . '\WooCommerce::woocommerce_single_product_image_thumbnail_html', 10, 2);
+      }
       // Adds additional images from product variation gallery.
       add_filter('woocommerce_product_get_gallery_image_ids', __NAMESPACE__ . '\WooCommerce::woocommerce_product_get_gallery_image_ids', 10, 2);
       add_action('woocommerce_product_thumbnails', __NAMESPACE__ . '\WooCommerce::woocommerce_product_thumbnails', 21);
